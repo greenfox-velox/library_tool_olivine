2cae07d61c672092018dddc820afbad0ef17147f
timikurucz <kurucztm@gmail.com>
2016-08-04 15:09:18 +0200

    Refresh

--- src/main/java/com/greenfox/olivine/librarytool/Application.java
+++ src/main/java/com/greenfox/olivine/librarytool/Application.java
@@ -5,6 +5,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class Application {
+    public static void uj(){
+        System.out.println("hello");
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
