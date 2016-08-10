f8374e73e87d9dd0d213c4eff8153361f1551890
attilakrupl <krupl.attila@gmail.com>
2016-08-10 14:39:23 +0100

    registration endpoint with mysql operating

--- src/main/java/library/Application.java
+++ src/main/java/library/Application.java
@@ -1,12 +1,14 @@
-package com.greenfox.olivine.librarytool;
+package library;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.net.URISyntaxException;
+
 @SpringBootApplication
 public class Application {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws URISyntaxException {
         SpringApplication.run(Application.class, args);
     }
 }
---
+++ src/main/java/library/JDBCTemplate/JDBCTemplate.java
@@ -0,0 +1,85 @@
+package library.JDBCTemplate;
+
+import com.library.user.User;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+
+import javax.sql.DataSource;
+//import java.net.URI;
+//import java.net.URISyntaxException;
+
+@Configuration
+public class JDBCTemplate {
+
+    private DataSource dataSource;
+    private JdbcTemplate jdbcTemplateObject;
+
+    public JDBCTemplate() {
+        dataSource = getDatasource();
+        jdbcTemplateObject = new JdbcTemplate(dataSource);
+    }
+
+
+//    public DriverManagerDataSource getDatasource() throws URISyntaxException {
+//        DriverManagerDataSource dataSource = new DriverManagerDataSource();
+//        URI dbUri = new URI(System.getenv("CLEARDB_DATABASE_URL"));
+//        String username = dbUri.getUserInfo().split(":")[0];
+//        String password = dbUri.getUserInfo().split(":")[1];
+//        String dbUrl = "jdbc:mysql://" + dbUri.getHost() + dbUri.getPath();
+//        dataSource.setDriverClassName("com.mysql.cj.jdbc.Driver");
+//        dataSource.setPassword(password);
+//        dataSource.setUrl(dbUrl);
+//        dataSource.setUsername(username);
+//        return dataSource;
+//    }
+
+
+    public DriverManagerDataSource getDatasource() {
+        DriverManagerDataSource dataSource = new DriverManagerDataSource();
+        dataSource.setDriverClassName("com.mysql.cj.jdbc.Driver");
+        dataSource.setPassword("fa709e80");
+        dataSource.setUrl("jdbc:mysql://eu-cdbr-west-01.cleardb.com/heroku_1230023ab8a6ac8?reconnect=true");
+        dataSource.setUsername("b32ab3afad4bf7");
+        return dataSource;
+    }
+
+
+//    public String addUser(User user){
+//        if (isUsernameFree(user.getUserName()) && passwordCheck(user)) {
+//            String SQL = "insert into users (email, firstname, lastName, role, userName, password) values (?, ?, ?, ?, ?, ?)";
+//            jdbcTemplateObject.update(SQL, user.getEmail(), user.getFirstName(), user.getLastName(), user.getRole(), user.getUserName(), user.getPassword());
+//            return "welcome";
+//        }   else {
+//            return "register";
+//        }
+//    }
+
+
+    public String registerUser(User user) {
+       if (isUsernameFree(user.getUserName()) && passwordCheck(user)) {
+           String SQL = "insert into users (email, firstname, lastName, role, userName, password) values (?, ?, ?, ?, ?, ?)";
+           jdbcTemplateObject.update(SQL, user.getEmail(), user.getFirstName(), user.getLastName(), user.getRole(), user.getUserName(), user.getPassword());
+           return "welcome";
+       } else {
+           return "registration";
+       }
+    }
+
+
+    public boolean isUsernameFree(String userName) {
+        for (User user : jdbcTemplateObject.query("select * from users", new UserMapper())) {
+            if (user.getUserName().equals(userName)) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+
+    public boolean passwordCheck(User user) {
+        return user.getPassword().equals(user.getConfPassword());
+    }
+
+
+}
