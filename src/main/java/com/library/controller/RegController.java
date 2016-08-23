5c6fbab28d7350d9bdd6dce30b0c1caddb375c02
timikurucz <kurucztm@gmail.com>
2016-08-23 14:49:02 +0200

    UserReq added back

--- src/main/java/com/library/controller/RegController.java
+++ src/main/java/com/library/controller/RegController.java
@@ -1,6 +1,7 @@
 package com.library.controller;
 
 import com.library.model.User;
+import com.library.userRequests.UserRequests;
 import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
