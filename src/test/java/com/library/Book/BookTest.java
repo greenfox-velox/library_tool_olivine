b7ad3bfa9886f7b775b35c4ecc75015f9b275b2e
timikurucz <kurucztm@gmail.com>
2016-09-08 16:00:46 +0200

    Added booktest

--- src/test/java/com/library/Book/BookTest.java
+++ src/test/java/com/library/Book/BookTest.java
@@ -11,47 +11,47 @@ public class BookTest {
 
     @Test()
     public void testgetBookTitle(){
-        assertEquals("getBookTitle should return title as a valid String", "Big Bad Book", greenfox.getbookTitle());
+        assertEquals("getBookTitle should return title as a valid String", "Big Bad Book", greenfox.getBookTitle());
     }
     @Test()
     public void testgetbookPublishedIn(){
-        assertEquals("getbookPublishedIn should return the year of publishing as a valid Integer", Integer.valueOf(2016) , greenfox.getbookPublishedIn());
+        assertEquals("getbookPublishedIn should return the year of publishing as a valid Integer", Integer.valueOf(2016) , greenfox.getBookPublishedIn());
     }
     @Test()
     public void testgetbookAuthor(){
-        assertEquals("getbookAuthor should return the author of the book a valid String", "Big Bad Wolf", greenfox.getbookAuthor());
+        assertEquals("getbookAuthor should return the author of the book a valid String", "Big Bad Wolf", greenfox.getBookAuthor());
     }
     @Test()
     public void testgetbookEdition(){
-        assertEquals("getbookEdition should an empty String" , "", greenfox.getbookEdition());
+        assertEquals("getbookEdition should an empty String" , "", greenfox.getBookEdition());
     }
     @Test()
     public void testgetbookCover(){
-        assertEquals("getbookCover should an empty String" , "", greenfox.getbookCover());
+        assertEquals("getbookCover should an empty String" , "", greenfox.getBookCover());
     }
     @Test()
     public void testgetbookISBNno(){
-        assertEquals("getbookISBNno should 0", Integer.valueOf(0), greenfox.getbookISBNno());
+        assertEquals("getbookISBNno should 0", Integer.valueOf(0), greenfox.getBookISBNno());
     }
 
     @Test()
     public void testgetbookFormat(){
-        assertEquals("getbookFormat should return an empty String" , "", greenfox.getbookFormat());
+        assertEquals("getbookFormat should return an empty String" , "", greenfox.getBookFormat());
     }
 
     @Test()
     public void testgetbookNumberOfPages(){
-        assertEquals("getbookNumberOfPages should return 0" , Integer.valueOf(0), greenfox.getbookNumberOfPages());
+        assertEquals("getbookNumberOfPages should return 0" , Integer.valueOf(0), greenfox.getBookNumberOfPages());
     }
 
     @Test()
     public void testgetbookInLanguage(){
-        assertEquals("getbookInLanguage should return 'English' as a valid String" , "English", greenfox.getbookInLanguage());
+        assertEquals("getbookInLanguage should return 'English' as a valid String" , "English", greenfox.getBookInLanguage());
     }
 
     @Test()
     public void testgetbookDescription(){
-        assertEquals("getbookDescription should return 'No description added yet' as a valid String" , "No description added yet", greenfox.getbookDescription());
+        assertEquals("getbookDescription should return 'No description added yet' as a valid String" , "No description added yet", greenfox.getBookDescription());
     }
 
     @Test()
